6bea3fc2cc79ad7e36413003ae650dceef6a5ec9
Elias <MagicElyas@users.noreply.github.com>
2018-12-13 23:50:58 +0100

    finish module crud, Testing Remains

--- pvtranslator/models/module.py
+++ pvtranslator/models/module.py
@@ -1,12 +1,30 @@
 from google.appengine.ext import db
 from pvtranslator.models.user import User
+from pvtranslator.models.auth import get_user
 
 
 class Module(db.Model):
-    id = db.IntegerProperty
     name = db.StringProperty
-    user_id = db.ReferenceProperty(User, collection_name='modules')
+    user = db.ReferenceProperty(User, collection_name='modules')
+
+    def has_permits(self):
+        user = get_user()
+        if user is not None and self.user.id == user.id:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def create_module(nombre):
+        return Module.get_or_insert(key_name=nombre, name=nombre, user=get_user())
+
+    @staticmethod
+    def delete_module(module):
+        db.delete(module)
+
+    @staticmethod
+    def edit_module(module):
+        module.put()
 
     def __repr__(self):
         return "Module(name='%s')" % self.name
-
