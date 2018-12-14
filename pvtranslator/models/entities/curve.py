fb2d170d591f860aa3eb579e81c008811d458419
MykexMP <MykexMP@hotmail.es>
2018-12-14 13:56:17 +0100

    CRUD curva finished

--- pvtranslator/models/entities/curve.py
+++ pvtranslator/models/entities/curve.py
@@ -1,5 +1,8 @@
 from google.appengine.ext import db
+from pvtranslator.models.utils.auth import get_user
 from pvtranslator.models.entities.campaign import Campaign
+from pvtranslator.models.entities.user import User
+from pvtranslator.models.utils import auth
 
 
 class Curve(db.Model):
@@ -9,7 +12,31 @@ class Curve(db.Model):
     i_values = db.ListProperty(float)
     p_values = db.ListProperty(float)
     campaign = db.ReferenceProperty(Campaign, collection_name='curves')
+    user = db.ReferenceProperty(User)
+
+    def has_permits(self):
+        user = auth.get_user()
+        if user is not None and self.campaign.user.id == user.id or self.campaign.module.user_id.id == user.id:
+            return True
+        return False
+
+    @staticmethod
+    def create_curve(hora, listav, listai, listap, campana):
+        if Curve.has_permits():
+            return Curve.get_or_insert(key_name=hora+"_"+campana.name+"_"+campana.module.name, hour=hora,
+                                       v_values=listav, i_values=listai, p_values=listap, campaign=campana)
+        else:
+            return None
+
+    @staticmethod
+    def delete_curve(curva):
+        db.delete(curva)
+
+    @staticmethod
+    def edit_curve(curva):
+        curva.put()
 
     def __repr__(self):
-        return "<Curve(hour='" + str(self.hour) + "', campaign='" + str(self.campaign) + "')>"
+        return "<Curve(hour='" + str(self.hour) + "', campaign='" + str(self.campaign) + "', module='" + \
+               str(self.campaign.module) +"')>"
 
